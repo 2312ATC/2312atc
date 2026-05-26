b0efac2fb42db7fd370e7ec2b78f2c74b6599afa
Nathan Savage <nathan.savage@kaimai.co.uk>
2026-05-26 13:54:33 +0100

    changed email

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = '2312 Squadron Air Cadets';
 export const SITE_DESCRIPTION =
 	'2312 (Tavistock) Squadron Air Cadets – part of the Royal Air Force Air Cadet organisation. Serving young people aged 12–20 in the Tavistock area.';
-export const SQUADRON_EMAIL = '0c.2312@rafac.mod.gov.uk';
+export const SQUADRON_EMAIL = 'oc.2312@rafac.mod.gov.uk';
 export const PARADE_NIGHTS = 'Tuesday & Thursday, 1830–2100';
 export const SQUADRON_ADDRESS = 'Cadet Centre Tavistock, Battery Ln, Tavistock PL19 9AU, United Kingdom';
